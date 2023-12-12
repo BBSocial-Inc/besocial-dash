64b4d85d6da41f0c065fa754acb990c8c5deffd5
Cirlorm <akotosel6@gmail.com>
2023-12-12 08:51:51 +0000

    mod:

--- .next/server/chunks/2299.js
+++
@@ -1,5 +0,0 @@
-"use strict";exports.id=2299,exports.ids=[2299],exports.modules={22299:(e,t,r)=>{r.d(t,{f:()=>s});var o=r(35108),i=r(9885),n=r(43979);let a="horizontal",l=["horizontal","vertical"],d=(0,i.forwardRef)((e,t)=>{let{decorative:r,orientation:l=a,...d}=e,s=p(l)?l:a;return(0,i.createElement)(n.WV.div,(0,o.Z)({"data-orientation":s},r?{role:"none"}:{"aria-orientation":"vertical"===s?s:void 0,role:"separator"},d,{ref:t}))});function p(e){return l.includes(e)}d.propTypes={orientation(e,t,r){let o=e[t],i=String(o);return o&&!p(o)?Error(`Invalid prop \`orientation\` of value \`${i}\` supplied to \`${r}\`, expected one of:
-  - horizontal
-  - vertical
-
-Defaulting to \`${a}\`.`):null}};let s=d}};
--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -8,8 +8,6 @@ import { z } from "zod";
 
 import { columns } from "./components/columns";
 import { DataTable } from "./components/data-table";
-import { UserNav } from "./components/user-nav";
-import { taskSchema } from "./data/schema";
 import { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { ADMIN_USER } from "@/graphql";
