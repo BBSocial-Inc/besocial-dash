aacc4b37fba162f9141a089f2618a262d845198f
Hassan <hassanabbas@team.softpers.com>
2024-03-22 17:27:54 +0500

    enhance: work on the UI of the send notifications

--- app/dashboard/notification/page.tsx
+++ app/dashboard/notification/page.tsx
@@ -10,10 +10,18 @@ import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function TaskPage() {
   const [contentUrl, setContentUrl] = React.useState("");
-  const [message, setMessage] = React.useState("");
+  const [title, setTitle] = React.useState("");
+  const [body, setBody] = React.useState("");
 
   const [notificationType, setNotificationType] = useState("marketing");
 
@@ -26,56 +34,72 @@ export default function TaskPage() {
   }
 
   return (
-    <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
-      {/* For selecting the type of notification */}
-      <div className="flex items-center space-x-2">
-        <p className="text-sm font-medium">Notificatiion Type</p>
-        <Select
-          value={notificationType}
-          onValueChange={handleNotificationTypeChange}
-        >
-          <SelectTrigger className="h-8 w-[100px]">
-            <SelectValue placeholder="Select notification type" />
-          </SelectTrigger>
-          <SelectContent side="top">
-            {['marketing', 'content'].map((notificationType) => (
-              <SelectItem key={notificationType} value={notificationType}>
-                {notificationType.charAt(0).toLocaleUpperCase() + notificationType.slice(1)}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-      </div>
-      <div>
-        {notificationType === "content" && (
-          <Input
-            id="content-url"
-            placeholder="content url ..."
-            type="text"
-            autoCapitalize="none"
-            autoCorrect="off"
-            // disabled={loading}
-            onChange={(e) => setContentUrl(e.target.value)}
-            value={contentUrl}
-          />
-        )}
-        <Input
-          id="message"
-          placeholder="message ..."
-          type="text"
-          autoCapitalize="none"
-          autoCorrect="off"
-          // disabled={loading}
-          onChange={(e) => setMessage(e.target.value)}
-          value={message}
-        />
-        <Button
-          // disabled={loading}
-          onClick={handleSendNotifications}
-        >
-          Send Notifications
-        </Button>
-      </div>
+    <div className="flex flex-col items-center space-y-8 p-8">
+      <Card className="w-full md:max-w-lg">
+        <CardHeader>
+          <CardTitle>Send Bulk Notifications</CardTitle>
+        </CardHeader>
+        <CardContent>
+          {/* For selecting the type of notification */}
+          <div className="flex items-center space-x-2">
+            <p className="text-sm font-medium">Notification Type</p>
+            <Select
+              value={notificationType}
+              onValueChange={handleNotificationTypeChange}
+            >
+              <SelectTrigger className="w-40">
+                <SelectValue placeholder="Select notification type" />
+              </SelectTrigger>
+              <SelectContent side="top">
+                {['marketing', 'content'].map((notificationType) => (
+                  <SelectItem key={notificationType} value={notificationType}>
+                    {notificationType.charAt(0).toUpperCase() + notificationType.slice(1)}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+          <div>
+            <Input
+              id="title"
+              placeholder="Title..."
+              type="text"
+              autoCapitalize="none"
+              autoCorrect="off"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              className="mt-4"
+            />
+            <Input
+              id="body"
+              placeholder="Body..."
+              type="text"
+              autoCapitalize="none"
+              autoCorrect="off"
+              onChange={(e) => setBody(e.target.value)}
+              value={body}
+              className="mt-4"
+            />
+            {notificationType === "content" && (
+              <Input
+                id="content-url"
+                placeholder="Content URL..."
+                type="text"
+                autoCapitalize="none"
+                autoCorrect="off"
+                onChange={(e) => setContentUrl(e.target.value)}
+                value={contentUrl}
+                className="mt-4"
+              />
+            )}
+            <div className="mt-4 flex justify-end">
+              <Button onClick={handleSendNotifications}>
+                Send Notifications
+              </Button>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
